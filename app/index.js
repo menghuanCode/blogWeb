aa09cf609a30edb4c74e95d8a251bcbbd62eb763
menghuan <1572482184@qq.com>
2020-01-03 00:38:07 +0800

    blogWeb

---
+++ app/index.js
@@ -0,0 +1,8 @@
+const Koa = require('koa')
+const koaStatic = require('koa-static')
+const app = new Koa()
+const path = require('path')
+
+app.use(koaStatic(path.join(__dirname, 'public')))
+
+app.listen(3001, () => console.log('程序监听 3001 端口，程序一起动'))
